7cab08d3763240fadc0ef944c7b42bc06529d9cd
Kauã <sousakaua7@gmail.com>
2023-08-12 23:32:06 -0300

    Formulário

---
+++ src/form/Select.jsx
@@ -0,0 +1,19 @@
+import styles from './Select.module.css'
+
+function Select({ name, text, options, handleOnChange, value }){
+    return(
+        <div className={styles.form_control}>
+            <label htmlFor={name}>{text}: </label>
+            <select name={name} id={name} onChange={handleOnChange} value={value || ''}>
+                <option>Selecione uma Opção: </option>
+                {options.map((options) => (
+                    <option value={options.id} key={options.id}>
+                        {options.name}
+                    </option> 
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default Select
